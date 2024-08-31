faf9e0144bd7d8ac496fbc22aca9d1a6251a912c
aoizora <mathesis@inbox.ru>
2024-08-31 08:48:03 +0200

    Первое окно

--- src/main/java/com/aoizora/book/Main.java
+++ src/main/java/com/aoizora/book/Main.java
@@ -1,9 +1,19 @@
 package com.aoizora.book;
 
-public class Main
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
 {
     public static void main(String[] args)
     {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("Hello, World!");
+        stage.show();
     }
 }
